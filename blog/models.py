42c4e4c14bd07ee8ecd87598fb188bc3e7dd9e59
Ashishansurkar <56146276+Ashishansurkar@users.noreply.github.com>
2020-01-04 16:24:54 +1300

    almost over

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,9 @@ class blog(models.Model):
     title = models.CharField(max_length=100)
     pub_date = models.DateField(auto_now = False)
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
+        
+    def summary(self):
+        return self.body[:100]
--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'myportfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'myportfolio/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +122,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myportfolio/static/')
+]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'satic')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
