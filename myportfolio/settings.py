8b8dce9234f9368f53edc967cf0070091c4e9000
Ashishansurkar <56146276+Ashishansurkar@users.noreply.github.com>
2020-01-04 17:06:22 +1300

    new comm

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'myportfolio/static/')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'satic')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
